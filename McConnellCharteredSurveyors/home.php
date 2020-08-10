1f7c6e131c0cd9f72fae9f61fe3af5013c84dc15
JamesClarkInDevelopment <69462964+JamesClarkInDevelopment@users.noreply.github.com>
2020-08-10 13:17:17 +0100

    Add files via upload

---
+++ McConnellCharteredSurveyors/home.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<title>McConnell Chartered Surveyors</title>
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="styles/styles.css">
+		
+		<style>
+			
+		
+		</style>
+		
+	</head>
+	
+	<body class="backgroundhome">
+		
+		<div class="container-fluid" style="width:80%">
+			<div class="row justify-content-md-center">
+				<div class ="col justify-content-center" >
+					<button type="button" class="btn btn-outline-light" style="width:100%; height:100%">
+					<a class ="" href="home.php" > <img src='images/header.jpg' style="width:100%; height:100%"></a></button>
+				</div>
+			</div>
+			<div class="row justify-content-md-center">
+				<div class ="col justify-content-center" >
+						<nav class="navbar navbar-expand-lg navbar-light bg-light">
+							  <a class="navbar-brand" href="home.php" id="navlogo">  </a>
+									<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="navbar-toggler-icon"></span>
+									</button>
+								<div class="collapse navbar-collapse" id="navbarNavDropdown">
+								<ul class="navbar-nav">
+								  <li class="nav-item active">
+									<a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="about.php">About</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="people.php">People</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="services.php">Services</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="propertysearch.php">Property Search</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="publications.php">Publications</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="news.php">News</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="contact.php">Contact</a>
+								  </li>
+								</ul>
+							  </div>
+						</nav>
+				</div>
+			</div>
+			<div  class="row justify-content-md-center" >
+				<div class="col justify-content-center ">
+					 <div class="box">
+						<img src='images/image1.jpg' style="width:100%"  >
+						<div class="text">
+							<h5>McConnell Chartered Surveyors advise a wide range of </br>
+							clients on all aspects of commercial and residential property </br>
+							throughout Northern Ireland</h5>
+							<p>
+								Commercial Property Sales<br />
+								Lettings and Acquisitions<br />
+								Commercial Property Management<br />
+								Landlord & Tenant Lease Negotiation<br />
+								Property Valuation<br />
+								Investment Advice<br />
+								Development<br />
+								Rating<br />
+								Residential & Ground Rent Management<br /> 
+							</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			
+			<div  class="row justify-content-md-center" id="filler">
+			</div>
+			
+			
+			<div  class="row justify-content-md-center" >
+				
+				<div class="col-7 justify-content-center " id="threebox">
+				<button type="button" class="btn btn-outline-warning">
+					<h4 class =" text-dark text-center font-italic font-weight-bold">COVID-19 UPDATE </h4>
+					<p style="color:black"  class="font-weight-bold">Important Contact Information</p><p> 
+					McConnell Chartered Surveyors Offices are now closed. Our teams can be reached during normal office hours via Email or Mobile - Please see "People" Page for Contacts. </br>
+					<p  class="font-weight-bold">Please note the following general contact details:</p><p>
+					Commerical Management Queries - info@mcconnellproperty.com </br>
+					Commercial management EMERGENCY ONLY 07534 656 347 </br>
+					Residential Management Queries -  residential.repairs@mcconnellproperty.com  </br>
+					Residential Management EMERGENCY ONLY 07957 975 594 </br>
+					<p class="font-weight-bold">Ulster Garden Villages Tennants: </p> <p>
+					General Queries - (Mon-Fri 9am-5pm) Call 07900 483 348 </br>
+					After Hours EMERGENCY ONLY - Fold Telecare 028 90 421 010 </br>
+					<p class="font-weight-bold">For other general queries:</p> <p>
+					Ground rent queries - ground.rent@mcconnellproperty.com</br>
+					Client Accounts - accounts.payable@mcconnellproperty.com</br>
+					Credit Control - credit.control@mcconnellproperty.com</br>
+					Raters - rates.management@mcconnellproperty.com</br> 
+					Property INsurance - insurance@mcconnellproperty.com</br> 
+					Suppplier/ Contracer invoices can be sent to management.invoices@mcconnellproperty.com</br>
+					</p>
+				</div>
+				</button>
+				<div  id="widthfiller">
+				</div>
+				<div class="col-4 justify-content-center " id="threebox">
+					<p> Add pictures </p>
+				</div>
+				
+			</div>
+			<div  class="row justify-content-md-center" id="filler">
+			</div>
+			
+			<div  class="row justify-content-md-center" >
+				<div class="col-11 justify-content-center " id="threebox">
+					<p> Add pictures </p>
+				</div>
+			</div>
+			<div  class="row justify-content-md-center" id="filler">
+			</div>
+			
+		
+		</div>	
+
+
+		
+			
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	</body>
+</html>
